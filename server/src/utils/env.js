3c109cbd6cbc9e0b846ca4790724bdb5412498c7
Rodolpho Rapôso <raposotiweb@gmail.com>
2025-11-02 16:12:04 -0300

    Atualização do projeto

--- server/src/utils/env.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-const env = {
-  PORT: process.env.PORT ? Number(process.env.PORT) : 3333,
-  CORS_ORIGIN: process.env.CORS_ORIGIN || '*'
-};
-
-export default env;
