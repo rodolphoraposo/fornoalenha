8ba1a46f67e23e464f78f056ff11dbcd96fe2e9c
Rodolpho Rapôso <raposotiweb@gmail.com>
2026-01-31 18:18:16 -0300

    feat: atualização do cardápio.

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,9 +40,9 @@ const SABORES = {
   ],
   Doce: [
     { name: "Brigadeiro", imagePath: "../imgs/pizzas/brigadeiro.webp" },
-    { name: "M&M", imagePath: "../imgs/pizzas/m&m.webp" },
+    // { name: "M&M", imagePath: "../imgs/pizzas/m&m.webp" },
     { name: "Banana", imagePath: "../imgs/pizzas/banana.webp" },
-    { name: "Romeu e Julieta", imagePath: "../imgs/pizzas/romeuejulieta.webp" }
+    // { name: "Romeu e Julieta", imagePath: "../imgs/pizzas/romeuejulieta.webp" }
   ]
 };
 
