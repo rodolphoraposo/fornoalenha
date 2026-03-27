c065c9550a4990d37fe398e36c4fcb7a67c541da
Rodolpho Rapôso <raposotiweb@gmail.com>
2026-03-27 18:53:13 -0300

    Disable promo popup by setting ativo to false

--- assets/js/promo-config.js
+++ assets/js/promo-config.js
@@ -6,7 +6,7 @@
 const PROMOCAO_CONFIG = {
   // ===== POP-UP DA TELA INICIAL =====
   popup: {
-    ativo: true,
+    ativo: false,
     mostrarCTA: false, // 👈 false oculta, true exibe
     imagem: "./assets/imgs/promocao/promo-hoje.png", 
     produto: "Pizza Grande - Sabores Tradicionais",
